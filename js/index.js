7787f0a0681eb7a82b12c76d585cfcaaf2d8eb62
Alexander Shmidt <shmidt.alex@gmail.com>
2018-08-02 10:06:39 +0300

    added barrel pic, the code of changing angle of barrel is written, going to split big object Game up into several different objects

--- js/index.js
+++ js/index.js
@@ -4,27 +4,30 @@ let Game = {
     canvas : undefined,
     canvasContext : undefined,
     backgroundSprite : undefined,
+    targetSprite: undefined,
     cannonBarrelSprite: undefined,
     balloonSprite2 : undefined,
     balloonSprite3 : undefined,
-    cannonPosition : { x : 460, y : 0 },
-    cannonOrigin: { x:34, y: 34},
-    //cannonImageSize:  {width:30, height: 30},
+    mousePostion: {x: 0, y: 0},
+    targetPosition : { x : 460, y : 0 },
+    targetOrigin: { x:34, y: 34},
+    cannonPosition: {x: 72, y: 405},
+    cannonOrigin: {x: 34, y: 43},
+    cannonRotation: 0,
 };
-//console.log(Game.cannonImageSize);
 
 Game.start = function () {
     Game.canvas = document.getElementById("mycanvas");
     Game.canvasContext = Game.canvas.getContext("2d");
     Game.backgroundSprite = new Image();
     Game.backgroundSprite.src = "spr_background.jpg";
+    Game.targetSprite = new Image();
+    Game.targetSprite.src = "target_PNG17.png";
     Game.cannonBarrelSprite = new Image();
+    Game.cannonBarrelSprite.src = "spr_cannon_barrel.png";
     Game.balloonSprite2 = new Image();
     Game.balloonSprite3 = new Image();
-
-    Game.cannonBarrelSprite.src = "target_PNG17.png";
-    //Game.cannonBarrelSprite.backgroundSize = '10px';
-   // console.log(Game.cannonBarrelSprite.height);
+    
     window.setTimeout(Game.mainLoop, 500);
 };
 
@@ -34,10 +37,10 @@ Game.clearCanvas = function () {
     Game.canvasContext.clearRect(0, 0, Game.canvas.width, Game.canvas.height);
 };
 
-Game.drawImage = function (sprite, position, origin, imageSize) {
-  //console.log(imageSize.width, imageSize.height);
+Game.drawImage = function (sprite, position, rotation, origin) {
   Game.canvasContext.save();
   Game.canvasContext.translate(position.x, position.y);
+  Game.canvasContext.rotate(rotation);
   Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height,
       -origin.x, -origin.y, sprite.width, sprite.height);
  
@@ -51,19 +54,24 @@ Game.mainLoop = function() {
 };
 
 Game.update = function () {
+  let opposite = Game.mousePostion.y - Game.cannonPosition.y;
+  let adjacent = Game.mousePostion.x - Game.cannonPosition.x;
+  Game.cannonRotation = Math.atan2(opposite, adjacent);
     //let d = new Date();
-    //Game.cannonPosition.x = d.getTime() * 0.3 % Game.canvas.width;
+    //Game.targetPosition.x = d.getTime() * 0.3 % Game.canvas.width;
     Game.canvas.onmousemove = Game.handleMouseMove;
 };
 
 Game.draw = function () {
-    Game.drawImage(Game.backgroundSprite, { x : 0, y : 0 }, {x: 0, y:0}, {});
-    Game.cannonOrigin = { x: Game.cannonBarrelSprite.width/2, y: Game.cannonBarrelSprite.height/2};
-    Game.drawImage(Game.cannonBarrelSprite, Game.cannonPosition, Game.cannonOrigin, Game.cannonImageSize);
-    //console.log(Game.cannonImageSize.width);
+    Game.drawImage(Game.backgroundSprite, { x : 0, y : 0 }, 0, {x: 0, y:0});
+    Game.targetOrigin = { x: Game.targetSprite.width/2, y: Game.targetSprite.height/2};
+    Game.drawImage(Game.targetSprite, Game.targetPosition, 0, Game.targetOrigin);
+    Game.drawImage(Game.cannonBarrelSprite, Game.cannonPosition, Game.cannonRotation, Game.cannonOrigin)
+    //console.log(Game.targetOrigin);
     
 };
+//this one define mouse position for different goals:
 Game.handleMouseMove = function(e){
-  Game.cannonPosition = { x: e.pageX, y: e.pageY };
-  console.log(Game.cannonPosition);
+  Game.targetPosition = { x: e.pageX, y: e.pageY };
+  Game.mousePostion = { x: e.pageX, y: e.pageY };
 };
